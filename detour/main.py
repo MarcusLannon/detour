f6769571953207a094bf036cd342c9b1b84fd725
MarcusLannon <marcus.lannon@gmail.com>
2020-11-24 22:54:01 +0000

    add main file to do full run on gpx file

---
+++ detour/main.py
@@ -0,0 +1,18 @@
+"""Simple runner function that takes a gpx file and prints the
+closed roads on route.
+"""
+from detour.route import Route
+from detour.gpx import GPXParser
+from detour.track import Track
+
+
+def main(gpx_path):
+    with open(gpx_path, "r") as f:
+        data = f.read()
+    gpx = GPXParser(data)
+    track = Track(gpx.extract_trackpoints())
+    route = Route(track)
+    route.reduce_points()
+    route._get_incidents()
+    route.find_flags()
+    route.display_inline()
